15daf63638c20aa195c9a152b2e90c738b97e99c
x1ph3rr <ayush1998gaur@gmail.com>
2024-11-26 13:32:14 +0530

    frontend updated

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -12,4 +12,35 @@ function Header(){
         localStorage.removeItem("authToken");
         window.location.href = "/login";
     }
+
+    return (
+        <div>
+            <nav className="header bg-slate-200 flex justify-between itmes-center">
+                <div className="logo w-1/4 text-center">
+                    <NavLink to="/">Todo App</NavLink>
+                </div>
+                <div className="flex justify-between">
+                    {
+                        token ? (
+                            <div className="flex itmes-center justify-center">
+                                <p className="mr-5">Welcome, <span className="text-xl text-blue-800 capitalize"> {user.name} </span></p>
+                            </div>
+                        ) : (
+                            <ul className="flex justify-end gap-3 w-3/4 pr-6">
+                                <li>
+                                    <NavLink to="/login">Login</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to='/register'>Register</NavLink>
+                                </li>
+                            </ul>
+                        )
+                    }
+                </div>
+            </nav>
+            <Outlet />
+        </div>
+    );
 }
+
+export default Header;
--- frontend/src/context/TokenContext.js
+++ frontend/src/context/TokenContext.js
@@ -1,3 +1,4 @@
 import { createContext } from "react";
 const TokenContext = createContext(null)
+
 export default TokenContext;
